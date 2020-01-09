9171cfc98468b72e50fb4eeb02d04bb55cdd50ae
Liam Fisher <liam.fisher@arnoldclark.com>
2020-01-09 08:33:35 +0000

    Added example

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using Shared;
+using StreamJsonRpc;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Connecting to server...");
+
+            using (var stream = new NamedPipeClientStream(
+                ".",
+                "RPCTestPipe",
+                PipeDirection.InOut,
+                PipeOptions.Asynchronous
+            ))
+            {
+                await stream.ConnectAsync();
+                Console.WriteLine("Connected!");
+                var jsonRpc = JsonRpc.Attach<IServerService>(stream);
+
+                var message = await jsonRpc.GetHelloWorld();
+
+                Console.WriteLine($"Received message: {message}");
+                Console.WriteLine("Closing connection...");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using Shared;
+using StreamJsonRpc;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int clientId = 0;
+            while (true)
+            {
+                Console.WriteLine("Waiting for client to make a connection...");
+                var stream = new NamedPipeServerStream(
+                    "RPCTestPipe",
+                    PipeDirection.InOut,
+                    NamedPipeServerStream.MaxAllowedServerInstances,
+                    PipeTransmissionMode.Byte,
+                    PipeOptions.Asynchronous);
+
+                await stream.WaitForConnectionAsync();
+                Console.WriteLine("Client Connected!");
+
+                var jsonRpc = JsonRpc.Attach(stream, new Server());
+                await jsonRpc.Completion;
+
+                Console.WriteLine("Client disconnected");
+            }
+        }
+    }
+
+    class Server : IServerService
+    {
+        public async Task<string> GetHelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
